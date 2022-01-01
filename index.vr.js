ae963ac1e5742ddceaf3917d3756af697289dce3
sanpedro ezequiel <sanpedrofabianezequiel@gmail.com>
2022-01-01 19:02:14 -0300

    Create Class State and Interval

--- index.vr.js
+++ index.vr.js
@@ -7,25 +7,41 @@ import {
   View,
 } from 'react-vr';
 
+
+
+class Task extends React.Component {
+  render(){
+    return(
+      <Text style={{textAlign:'center',fontSize:0.2}} >{this.props.text}</Text>
+    )
+  }
+}
+
 export default class basics extends React.Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      showSign : true
+    }
+
+    setInterval(() => {
+      this.setState({showSign: !this.state.showSign})
+    }, 1000);
+  }
+  
   render() {
+    let mensaje = (this.state.showSign) ? 'Welcome VR' :'';
+
+
     return (
       <View>
         <Pano source={asset('starry-sky.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          Sofia Pastelito
-        </Text>
+        <View style={{transform:[{translate:[0,0,-3]}], layoutOrigin: [0.5,0.5]  }}>
+            <Text style={{fontSize:0.1,transform:[{translate:[-1,0,-2]}]}} >{mensaje}</Text>
+            <Task text='First item'/>
+        </View>
       </View>
     );
   }
